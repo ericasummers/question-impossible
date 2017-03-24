effed29ba3aec0c8aa3a3255ca524a77a6844030
Erica Wright <ericaw21@gmail.com>
2017-03-24 10:20:41 -0700

    create question-detail component to display question and delete button on individual page

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,12 +13,6 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    },
-    delete(question) {
-      if (confirm('Are you sure you want to delete this question?')) {
-        question.destroyRecord();
-        this.transitionTo('index');
-      }
     }
   }
 });
