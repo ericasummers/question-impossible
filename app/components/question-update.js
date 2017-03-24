80d3b80575b128900a0f9eaa66217ee47bd0c8bd
Erica Wright <ericaw21@gmail.com>
2017-03-24 10:36:35 -0700

    create question-update component to edit questions

---
+++ app/components/question-update.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateQuestionShowing: false,
+  actions: {
+    showUpdateForm() {
+      this.set('updateQuestionShowing', true);
+    },
+    cancelUpdate() {
+      this.set('updateQuestionShowing', false);
+    },
+    update(question) {
+      var params = {
+        body: this.get('body'),
+        author: this.get('author'),
+        date: this.get('date')
+      };
+      this.set('updateQuestionShowing', false);
+      this.sendAction('update', question, params);
+    }
+  }
+});
