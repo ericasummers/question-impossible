8a893b916cda609db945b3874ecd35f3e174a980
Erica Wright <ericaw21@gmail.com>
2017-03-24 10:08:25 -0700

    add functionality to delete question

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,6 +13,12 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
+    },
+    delete(question) {
+      if (confirm('Are you sure you want to delete this question?')) {
+        question.destroyRecord();
+        this.transitionTo('index');
+      }
     }
   }
 });
